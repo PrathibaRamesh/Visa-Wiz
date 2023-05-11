277fd6a8ab117be7ec08826cc9fec311a6dc12fb
Elahe87 <117861889+Elahe87@users.noreply.github.com>
2023-05-11 11:42:47 -0700

    new changes

--- src/Wage.js
+++ src/Wage.js
@@ -16,18 +16,18 @@ export default class Wage extends Component {
     render() {
         return (
             <div>
-                <Navbar bg="dark" variant="dark" expand="lg" style={{ width: "100%", paddingLeft: "1%", paddingRight: "50%" }}>
+                <Navbar  expand="lg" style={{ width: "100%", paddingLeft: "1%", paddingRight: "50%", backgroundColor:'#42276a' }}>
                     <Container style={{  width: '200px'}}>
-                        <Navbar.Brand href="/" style={{ fontSize: '30px', marginLeft: '1px' ,paddingRight:'120px'  }} >Visa-Wiz</Navbar.Brand>
+                        <Navbar.Brand href="/" style={{ fontSize: '30px', marginLeft: '1px' ,paddingRight:'120px', color:'white'  }} >Visa-Wiz</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto" >
-                                <Nav.Link href="/Occupation" style={{ fontSize: '20px', paddingRight: "5%" }} >Occupation</Nav.Link>
-                                <Nav.Link href="/Employer" style={{ fontSize: '20px',paddingRight: "5%"  }}>Employer</Nav.Link>
-                                <Nav.Link href="/YearCount" style={{ fontSize: '20px',paddingRight: "5%", whiteSpace: "nowrap" }}>Month & Year</Nav.Link>
-                                <Nav.Link href="/Salary" style={{ fontSize: '20px',paddingRight: "5%"  }}>Salary</Nav.Link>
-                                <Nav.Link href="/Wage" style={{ fontSize: '20px',paddingRight: "5%"  }}>Wage</Nav.Link>
-                                <Nav.Link href="/USState" style={{ fontSize: '20px',whiteSpace: "nowrap" }}>US States</Nav.Link>
+                                <Nav.Link href="/Occupation" style={{ fontSize: '20px', paddingRight: "5%", color:'white' }} >Occupation</Nav.Link>
+                                <Nav.Link href="/Employer" style={{ fontSize: '20px',paddingRight: "5%",color:'white'  }}>Employer</Nav.Link>
+                                <Nav.Link href="/YearCount" style={{ fontSize: '20px',paddingRight: "5%", whiteSpace: "nowrap", color:'white' }}>Month & Year</Nav.Link>
+                                <Nav.Link href="/Salary" style={{ fontSize: '20px',paddingRight: "5%" ,color:'white' }}>Salary</Nav.Link>
+                                <Nav.Link href="/Wage" style={{ fontSize: '20px',paddingRight: "5%",color:'white'  }}>Wage</Nav.Link>
+                                <Nav.Link href="/USState" style={{ fontSize: '20px',whiteSpace: "nowrap",color:'white' }}>US States</Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
